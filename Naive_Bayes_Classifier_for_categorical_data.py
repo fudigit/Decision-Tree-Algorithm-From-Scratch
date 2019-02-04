a29429df92b0652994b89f2fded155c91e0709dd
fudigit <30851222+fudigit@users.noreply.github.com>
2019-02-03 23:59:27 -0600

    Create Naive_Bayes_Classifier_for_categorical_data.py

---
+++ Naive_Bayes_Classifier_for_categorical_data.py
@@ -0,0 +1,6 @@
+
+#The alroghrim is basically counting to calculate required probability distribution for P(Y|X)
+
+# 1. Calculate the class probability: P(Y = y_i)
+# 2. Calculate the conditional probability(ies): P(X = x_i| Y = y_i)
+# 3. Time each P(X = x_i| Y = y_i)* P(Y = y_i)

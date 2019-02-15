02a99b20fc54d29a0cfd6afa599a1efe21cda18d
fudigit <30851222+fudigit@users.noreply.github.com>
2019-02-15 00:00:33 -0600

    Update Logistic_Regression.py

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -126,151 +126,68 @@ print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
 
 
 
-# load data
+
+
 import csv
-with open('Go_out_data.csv') as data_csv:
-    data_list = list(csv.reader(data_csv))
 
-header = data_list[0]
-data = data_list[1:]
-test_data = [row[:2] for row in data]
-
-# 1. Calculate the class probability: P(Y = y_i)
-# Helper function
-def value_unique(column, data):
-    return set(row[column] for row in data)
-    
-#--------------------------------------------------------------
-# calculate class probability and store in a dictionary, alternative way
-# is to calc on demand
-def class_prob(data,column):
-    prob_Y = {}
-    #labels = value_unique(data, column)
-    for row in data:
-        label = row[column]
-        if label not in prob_Y:
-            prob_Y[label] = 0
-        prob_Y[label] += 1/len(data)
-    return prob_Y
-        
-# 2. Calculate the conditional probability(ies): P(X = x_i| Y = y_i)
-
-# Try to creat a class for con_prob. 
-# The hope is con_prob.prob(y_i, x_i) give the conditional prob
-class con_prob:
-    def __init__(self, y_i, x_i, prob):
-        self.y_i = y_i
-        self.x_i = x_i
-        self.prob = prob
-#-----------------------------------------------------------------   
-
-def calc_prob(case_i, case_col, case_set):
-    '''calculate probability of occurrence of a feature value in a given dataset'''
-    size = len(case_set)
-    count = 0
-    for row in case_set:
-        if case_i in row[case_col]:
-            '''this does not consider feature value, but if the entire row contain the value'''
-            count += 1
-    prob = count/size
-    return prob
-
-print(calc_prob('go-out', 2, data))
-
-
-def calc_con_prob(x_i, x_col, y_i, y_col, data): 
-    '''calculate conditional prob'''
-    #filter dataset to subset by y_i
-    subset_yi = []
-    for row in data:
-        '''again, this does not consider feature value, but if the entire row contain the value'''
-        if y_i in row[y_col]:
-            subset_yi.append(row)
-    #calc prob in subset
-    con_prob = calc_prob(x_i, x_col, subset_yi)
-    return con_prob
-    
-#calc_con_prob('sunny', 'go-out', data)
-
-    
-
-# 3. Product them so P(Y|X) ~  P(X = x_i| Y = y_i)* P(Y = y_i), denominator P(X) is ignored 
-# since if we want to classify Y, P(X) is just a scaling factor. True P(Y|X) = P(X|Y)*P(Y)/P(X)
-
-#probability of one case
-def prob(test_row, y_i):
-    prod_condi_prob = 1
-    y_col = len(test_row)
-    for i in range(len(test_row)):
-        prod_condi_prob *= calc_con_prob(test_row[i], i, y_i, y_col, data)
-    class_prob = calc_prob(y_i, y_col, data)
-    
-    bayes_est = prod_condi_prob*class_prob
-    return bayes_est
-#predict(test_data[3], 'go-out')
-
-
-# 4. predict prob all cases:
-for test_row in test_data:
-    for y_i in value_unique(2, data):
-        print(test_row, y_i, prob(test_row, y_i))
-
-#calculate probability distribution:
-def joint_prob_dist(data):
-    for i in value_unique(0,data):
-        for j in value_unique(1,data):
-            for k in value_unique(2,data):
-                print('P(L=%s|w = %s, c = %s)' %(k, i, j), '=',
-                 prob([i,j],k))
-
-
-joint_prob_dist(data)
-
-
-# predicion of one case
-def classify(test_row):
-  label = None
-  max_prob = 0
-  y_col = len(data[0]) - 1
-  #find the y_i with max probability
-  for y_i in value_unique(y_col, data):
-    yi_prob = prob(test_row, y_i)
-    if yi_prob > max_prob:
-      max_prob, label = yi_prob, y_i
+'''Handle data'''
+def load_csv(filename):
+  lines = csv.reader(open(filename))
+  dataset = list(lines)
   
-  return label
-      
-print(classify(test_data[3]))
-  
+  for i in range(1, len(dataset)):
+    dataset[i] = [float(x) for x in dataset[i]]
 
-#prediction of all data
-def predict(test_data):
-  predictions = []
-  for test_row in test_data:
-    label = classify(test_row)
-    predictions.append(label)
-  return predictions
+  return dataset
 
-print(predict(test_data))
+filename= 'pima-indians-diabetes.csv'
+dataset= load_csv('diabetes.csv')
+print('loaded {} with {} rows'.format(filename,len(dataset)))
 
-#accuracy
-y_true = []
-y_col = len(data[0])-1
-for row in data:
-  y_true.append(row[y_col])
+'''split data'''
+import random
+def split_date(dataset, split_ratio):
+  trainSize = round(len(dataset)*split_ratio)
+  trainSet = []
+  testSet = []
+  random.shuffle(dataset) 
+  trainSet = dataset[:trainSize]
+  testSet= dataset[trainSize:]
+  return trainSet, testSet
 
+trainSet, testSet = split_date(dataset, 0.8)
 
-def accuracy(predictions, y_true):
-  count_true = 0
-  n_rows = len(data)
-  for i in range(n_rows):
-    if predictions[i] == y_true[i]:
-      count_true += 1
-  return count_true/n_rows
-
-print(accuracy(predict(test_data), y_true))
+print('split {} rows into train with {} and test with {}'.format
+(len(dataset),len(trainSet),len(testSet)))
 
 
-      
-  
-  
+
+
+
+
+import math
+
+def predict(row, coefficients):
+  yhat = coefficients[0]
+  for i in range(len(row)-1):
+    yhat += coefficients[i+1]*row[i]
+  return 1/(1+math.exp(-yhat))
+
+
+dataset = [[2.7810836,2.550537003,0],
+	[1.465489372,2.362125076,0],
+	[3.396561688,4.400293529,0],
+	[1.38807019,1.850220317,0],
+	[3.06407232,3.005305973,0],
+	[7.627531214,2.759262235,1],
+	[5.332441248,2.088626775,1],
+	[6.922596716,1.77106367,1],
+	[8.675418651,-0.242068655,1],
+	[7.673756466,3.508563011,1]]
+
+coef = [-0.406605464, 0.852573316, -1.104746259]
+
+
+for row in dataset:
+  yhat = predict(row, coef)
+  print("Expected=%.3f, Predicted=%.3f [%d]" % (row[-1], yhat, round(yhat)))

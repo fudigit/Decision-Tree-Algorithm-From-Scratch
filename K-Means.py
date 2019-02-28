39fc0a453a831f02d4e8202258b4f0d6b0fb27da
fudigit <30851222+fudigit@users.noreply.github.com>
2019-02-28 17:40:54 -0600

    Update K-Means.py

--- K-Means.py
+++ K-Means.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 28 22:21:55 2018
-
 @author: Di
 """
-
+'''
 try:
     import plotly
     from plotly.graph_objs import Scatter, Scatter3d, Layout
 except ImportError:
     plotly = Scatter = Scatter3d = Layout = None
     print('INFO: Plotly is not installed, plots will not be generated.')
-
+'''
 
 '''# pure python inplementation of K-means clustering'''
 
@@ -19,7 +18,6 @@ except ImportError:
 repeat 2 steps until convergence
 step 1: Assignment: for each point, get the distance to each controid, assign point to closest centroid's cluster
 step 2: Update: recalculate the centroid of the cluster, it will be the centroid of the new cluster
-
 '''
 
 
@@ -38,7 +36,10 @@ class Point(object):
     def __repr__(self):
         return str(self.coords)
     
-a = Point([1])
+point_a = Point([1])
+print(point_a)
+point_a.coords.append(2)
+print(point_a)
 
 class Cluster(object):
     '''a set of points and their contriod'''
@@ -102,13 +103,7 @@ class Cluster(object):
         return dis_total
             
 
-# test update(self, points)   
-#a = Cluster(Points1)
-#print(a)
-#print(a.update(Points2))
-#print(a)
-        
-
+################make testing of Points work!#####################
 '''Helper Method'''
 
 import random
@@ -125,14 +120,16 @@ def make_random_point(n_dims, lower, upper):
 # test
 make_random_point(2, 0, 10)
 #Generate somes points
-Points1 = [
-        make_random_point(2, 0, 10) for _ in range(30)
+points1 = [
+        make_random_point(2, 0, 10) for _ in range(10)
         ]
 
 
-Points2 =  [
-        make_random_point(2, 10, 20) for _ in range(30)
+points2 =  [
+        make_random_point(2, 20, 30) for _ in range(10)
         ]
+
+points3 = [Point([_]) for _ in range(10)]
 
 
 def get_distance(a,b):
@@ -148,174 +145,83 @@ def get_distance(a,b):
 # test
 a, b = Point([1,1,1]), Point([2,2,2])
 get_distance(a, b)
+#######################################
 
+# test cluster
+#cluster_test = Cluster([Point([1,1,1])])
+#print(cluster_test.centroid)
+#cluster_test.points.append(Point([2,2,2]))
+#print(cluster_test.centroid)
+#print(cluster_test.points[0],'1st point')
 
+# test update(self, points)   
+#a = Cluster(Points1)
+#print(a)
+#print(a.update(Points2))
+#print(a)
+
+def calculate_error(clusters):
+  '''average Euclidean distance between points and centroid'''
+  total_EuDis, num_points = 0, 0
+  for c in clusters:
+    total_EuDis += c.get_total_distance()
+    num_points += len(c.points)
+  avg_total_dis = total_EuDis/num_points
+  return avg_total_dis
 
 
 
 '''define kMeans method'''
 def kmeans(points, k, cuff):
+  
+  '''initializing centroids and clusters'''
     # pick out k random points to use as initial centriods
-    initial_centroids = random.sample(points, k)
-    
+  initial_centroids = random.sample(points, k)
     # create k clusters using the centriods
     # Note: Cluster take list!
     # How to create and store multiple clusters?
-    Clusters = [Cluster([init_c]) for init_c in initial_centroids]
-    
-    # assignment step
-    
-    
-    # update step
-    
-
-
-
-
-
-
-
-
-num_points = 20
-
-dimension = 2
-
-lower = 0
-upper = 200
-
-num_clusters = 3
-
-cutoff = 0.2
-
-
-
-class Point(object):
-  def __init__(self,coords):
-    self.coords = coords
-    self.n = len(coords)
+  clusters = [Cluster([init_c]) for init_c in initial_centroids]
   
-  def __repr__(self):
-    return str(self.coords)
+  total_shift = None
+  
+  while total_shift == None or total_shift > cuff:
+  # loop until the centroid stablize
+    '''clear cluster'''
+    # 1. Clear the initial centroid Point
+    # 2. cluster full Due to previous assignment, clearance the list for new Points. (not deleting the list!) 
+    for cluster in clusters:
+      del cluster.points[:]
 
-'''define cluster class'''
-class Cluster(object):
-    def __init__(self, points):
-        
-        if len(points) == 0:
-            raise Exception('ERROR: empty cluster')
-            
-        self.points = points
-        
-        self.n = points[0].n
-        
-        for p in points:
-            if p.n != self.n:
-                raise Exception('ERROR: incosistent dimentions')
-                
-        self.centroid = self.calculate_centroid()
-        
-    def __repr__(self):
-        return str(self.points)
-    
-    def update(self, points):
-        old_centroid = self.centroid
-        self.points = points
-        
-        if len(self.points) == 0:
-            return 0 
-        self.centroid = self.calculate_centroid()
-        shift = get_distance(old_centroid, self.centroid)
-        return shift
-        
-    def calculate_centroid(self):
-        num_points = len(self.points)
-        
-        coords = [p.coords for p in self.points]
-        unzipped = zip(*coords)
-        centroid_coords = [math.fsum(dList) / num_points for dList in unzipped]
-        
-        return Point(centroid_coords)
-    
-    def get_total_distance(self):
-        sum_of_distances = 0.0
-        for p in self.points:
-            sum_of_distances += get_distance(p, self.centroid)
-
-        return sum_of_distances
-
-''' Helper Methods'''
-def get_distance(a,b):
-    if a.n != b.n:
-        raise Exception('Error: non comparable points')
-        
-    accumulated_difference = 0.0
-    for i in range(a.n):
-        square_difference = pow((a.coords[i] - b.coords[i]),2)
-        accumulated_difference += square_difference
-        
-    return accumulated_difference
-
-def make_random_point(dimension, lower, upper):
-  p = Point([random.uniform(lower,upper) for _ in range(dimension)])
-  return p
-
-
-
-'''k-means main code'''
-points = [
-  make_random_point(dimension, lower, upper) for i in range(num_points)
-]
-
-#print(points)
-
-#k random points oto use as initial centroids
-k = 3
-
-initial_centroids = random.sample(points,k)
-
-#print(initial_centroids)
-
-clusters = [Cluster([p]) for p in initial_centroids]
-
-print(clusters)
-
-loop_counter = 0
-
-'''tesing'''
-
-
-while True:
-    lists = [[] for _ in clusters]
-    cluster_count = len(clusters)
-    
-    loop_counter += 1
-    
+    '''assignment step:''' 
+    # assign each point to the closest cluster
     for p in points:
-         smallest_distance = get_distance(p, clusters[0].centroid)
-         print (smallest_distance)
-         cluster_index = 0
-         
-         for i in range(1, cluster_count):
-             distance = get_distance(p, clusters[i].centroid)
-             print (distance)
-             if distance < smallest_distance:
-                 smallest_distance = distance
-                 cluster_index = i
-         lists[cluster_index].append(p)
-        
-    biggest_shift = 0.0
+      closer_c = None
+      closer_dis = None 
+      for c in clusters:
+        dis = get_distance(p, c.centroid)
+        # find min dis and cloest cluster
+        if closer_dis == None or dis < closer_dis:
+          closer_dis, closer_c = dis, c
+      # appending to closer_c is appending to the target cluster!
+      closer_c.points.append(p)
+      #print(clusters)
     
+    '''update step'''
+    # calculate the new centroid for each cluster
+    total_shift = 0
+    # print([c.centroid for c in clusters])
+    for c in clusters:
+      # print(c.centroid)
+      # update calc new centroid, and return distance between old/new. Note the points are already in the cluster!
+      total_shift += c.update(c.points)
+    print([c.centroid for c in clusters])
+    #print(total_shift)
+  
+  return clusters, total_shift
 
-    for i in range(cluster_count):
-        shift = clusters[i].update(lists[i])
-        biggest_shift = max(biggest_shift, shift)
-        
-    clusters = [c for c in clusters if len(c.points) != 0]
-    
-    if biggest_shift < cutoff:
-        print('converged after %s iterations' % loop_counter)
-        break
-    
-    
-    
-print (clusters)
+
+result = kmeans(points1 + points2, 2, 0.1)[0]
+
+
+
+print(calculate_error(result))
